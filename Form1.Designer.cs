38867729aa27f5c4c1c7a314cb5cb4470f137bb3
abeny <abeny@AHMEDBENYUSUF>
2024-11-04 11:49:53 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace C05_P01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputListBox = new System.Windows.Forms.ListBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputListBox
+            // 
+            this.OutputListBox.BackColor = System.Drawing.SystemColors.Info;
+            this.OutputListBox.FormattingEnabled = true;
+            this.OutputListBox.ItemHeight = 20;
+            this.OutputListBox.Location = new System.Drawing.Point(22, 33);
+            this.OutputListBox.Name = "OutputListBox";
+            this.OutputListBox.Size = new System.Drawing.Size(284, 84);
+            this.OutputListBox.TabIndex = 0;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(22, 186);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(94, 30);
+            this.CalculateButton.TabIndex = 1;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(212, 186);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(94, 30);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 245);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.OutputListBox);
+            this.Name = "Form1";
+            this.Text = "Tuiton Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox OutputListBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿//Ahmed Benyusuf
+//Class and Section (CS 313 01)
+//Assignment (Program 01 Chapter 05)
+//Description of the Program : this program will calculate and display the cost of tuition as it rises by 2% for 5 years.
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C05_P01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            double Loops = 0; // loop counter
+            double Cost = 6000; // initial cost of tuition
+            OutputListBox.Items.Add("Cost for Year " + Loops + ":" + Cost.ToString("c"));
+            // Outputs the cost for the first year
+            Loops += 1; // increments the loop counter one time to start the for loop
+            for (Loops = 1; Loops < 6; Loops++) // for loop to continue for 5 years
+            {
+                Cost = Cost + (Cost * 0.02);// Calculates cost increases by 2% each year
+                OutputListBox.Items.Add("Cost for Year "+ Loops +":" + Cost.ToString("c"));
+                // Outputs the cost for each year
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            // Closes the program
+        }
+    }
+}
+
